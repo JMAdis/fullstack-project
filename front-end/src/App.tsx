a66b07287147a49075514bc0a5292cfe1e3704ae
J M Adis <jessicaadis7@gmail.com>
2024-02-12 16:19:58 +0000

    have added carousel to the home page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
 import "./App.scss";
 import Nav from "./components/Nav/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Carousel from "./components/Carousel/Carousel";
+import PlaceholderImage from "./assets/book cover placeholder.png"
 
 const App = () => {
   return (
     <Router>
       <Nav />
       <p>Hello World!</p>
+      <Carousel images={PlaceholderImage} />
     </Router>
   );
 };
--- front-end/src/components/Carousel/Carousel.tsx
+++ front-end/src/components/Carousel/Carousel.tsx
@@ -1,3 +1,39 @@
 import React, {useState} from "react";
 
-const Carousel 
+type CarouselProps = {
+    images: string;
+}
+
+const Carousel = ({images} : CarouselProps) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    
+    const nextSlide = () => {
+        setActiveIndex((prevIndex) => 
+        prevIndex === images.length -1 ? 0 : prevIndex + 1
+        );
+    };
+
+    const prevSlide = () => {
+        setActiveIndex((prevIndex) =>
+        prevIndex === 0 ? images.length -1 : prevIndex - 1
+        );
+    };
+
+    return (
+        <div className="carousel">
+            <button onClick={prevSlide} className="carousel__btn carousel__btn--prev">
+                &lt;
+            </button>
+            <img
+            src={images[activeIndex]}
+            alt={`Slide ${activeIndex}`}
+            className="carousel__img"
+            />
+            <button onClick={nextSlide} className="carousel__btn carousel__buton--next">
+                &gt;
+            </button>
+        </div>
+    );
+};
+
+export default Carousel;
