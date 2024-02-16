ff771aafca54b42b8c9a64d057331ca15833ea34
J M Adis <jessicaadis7@gmail.com>
2024-02-16 10:21:34 +0000

    updates to form

--- front-end/src/containers/Home/Home.tsx
+++ front-end/src/containers/Home/Home.tsx
@@ -1,13 +1,34 @@
 import BookRequest from "../../types/BookRequest";
 import Form from "../../components/Form/Form";
-import Carousel from "../../components/Carousel/Carousel";;
+import Carousel from "../../components/Carousel/Carousel";
 
 type HomeProps = {
   books: BookRequest[];
   onBookFormSubmit: (newBook: BookRequest) => void;
 };
 
+
 const Home = ({ books, onBookFormSubmit }: HomeProps) => {
+
+  const handleCreateBook = async (newBook: BookRequest) => {
+    const result = await fetch("http://localhost:8080/books", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBook),
+    });
+    
+    if (result.ok) {
+      alert("Book created successfully");
+      const createdBook = await result.json();
+      onBookFormSubmit(createdBook);
+    } else {
+      const message = await result.text();
+      alert(message);
+    }
+  };
+
   return (
     <section>
       <Form
@@ -24,7 +45,7 @@ const Home = ({ books, onBookFormSubmit }: HomeProps) => {
           format: "",
         }}
         formTitle={"What book are you reading?"}
-        handleSubmit={onBookFormSubmit}
+        handleSubmit={handleCreateBook}
       />
       <Carousel books={books} />
     </section>
